2f20abac8ef60e8da49abf82284cad536d60910e
Yuvraj Bhatia <bhatiayuvraj58@gmail.com>
2023-08-12 20:01:01 -0700

    Added prework files to repository

---
+++ Prework/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Yuvraj Bhatia on 05/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundcolor(_ sender: UIButton) {
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        func changeColor() -> UIColor{
+
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+            
+            
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            
+
+
+            }
+    }
+    
+}
+
